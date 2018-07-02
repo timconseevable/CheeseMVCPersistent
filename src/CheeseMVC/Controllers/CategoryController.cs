e3da3bab69a091edaf4a7a9096e93fc9368839d9
Tim Conceivable <headskape@yahoo.com>
2018-07-01 21:59:53 -0500

    started part 1

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
